bd61ba19b3b520678c5bfd5f7a0f638c5e1088b2
Cory Brunson <brunson@uchc.edu>
2016-11-23 09:41:51 -0500

    update example.R

--- scripts/example.R
+++ scripts/example.R
@@ -2,12 +2,12 @@
 library(bitriad)
 data(list = data(package = 'bitriad')$results[, 3])
 
-example <- list(DG1 = women.group,
-                DG2 = women.clique,
+example <- list(DG1 = women_group,
+                DG2 = women_clique,
                 BB = chicago1960s,
                 GWF = minneapolis1970s,
-                NMT1 = nmt.meetings,
-                NMT2 = nmt.organizations,
+                NMT1 = nmt_meetings,
+                NMT2 = nmt_organizations,
                 FH = whigs)
 
 save(example, file = 'calc/example.RData')
