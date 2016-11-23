3be2ef7c99950bce03b0d9fc461210c64bd0a315
Cory Brunson <brunson@uchc.edu>
2016-11-23 09:46:38 -0500

    restrict to fewer examples

--- scripts/example.R
+++ scripts/example.R
@@ -2,13 +2,15 @@
 library(bitriad)
 data(list = data(package = 'bitriad')$results[, 3])
 
-example <- list(DG1 = women_group,
-                DG2 = women_clique,
-                BB = chicago1960s,
-                GWF = minneapolis1970s,
-                NMT1 = nmt_meetings,
-                NMT2 = nmt_organizations,
-                FH = whigs)
+example <- list(
+  DG1 = women_group
+  , DG2 = women_clique
+  #, BB = chicago1960s
+  , GWF = minneapolis1970s
+  , NMT1 = nmt_meetings
+  , NMT2 = nmt_organizations
+  #, FH = whigs
+)
 
 save(example, file = 'calc/example.RData')
 
