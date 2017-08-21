bf5d3df5295e5d362f934a61149c717811510f21
Cory Brunson <brunson@uchc.edu>
2017-08-21 13:20:26 -0400

    update mathrev-census.r

--- scripts/mathrev-census.R
+++ scripts/mathrev-census.R
@@ -14,33 +14,36 @@ applied2 <- sprintf('%02d', 60:96)
 mathrev.applied <- mathrev[substr(mathrev$pclass, 1, 2) %in% applied2, ]
 mathrev.applied.census <- lapply(yrs, function(yr) {
   triad_census_an(as_an(paper.author.graph(mathrev.applied[
-    mathrev.applied$year %in% (yr - dur + 1):yr, ])), verbose = TRUE)
+    mathrev.applied$year %in% (yr - dur + 1):yr, ])))
 })
 mathrev.applied.unif.census <- lapply(mathrev.applied.census, function(census) {
-  project_census(census)$uniformity
+  project_census(census)$difference
 })
 save(mathrev.applied.census, mathrev.applied.unif.census,
      file = 'calc/mathrev-applied-census.RData')
+print('Applied done')
 
 mathrev.pure <- mathrev[substr(mathrev$pclass, 1, 2) %in% pure2, ]
 mathrev.pure.census <- lapply(yrs, function(yr) {
   triad_census_an(as_an(paper.author.graph(mathrev.pure[
-    mathrev.pure$year %in% (yr - dur + 1):yr, ])), verbose = TRUE)
+    mathrev.pure$year %in% (yr - dur + 1):yr, ])))
 })
 mathrev.pure.unif.census <- lapply(mathrev.pure.census, function(census) {
-  project_census(census)$uniformity
+  project_census(census)$difference
 })
 save(mathrev.applied.census, mathrev.pure.unif.census,
      file = 'calc/mathrev-pure-census.RData')
+print('Pure done')
 
 mathrev.census <- lapply(yrs, function(yr) {
   triad_census_an(as_an(paper.author.graph(mathrev[
-    mathrev$year %in% (yr - dur + 1):yr, ])), verbose = TRUE)
+    mathrev$year %in% (yr - dur + 1):yr, ])))
 })
 mathrev.unif.census <- lapply(mathrev.census, function(census) {
-  project_census(census)$uniformity
+  project_census(census)$difference
 })
 save(mathrev.census, mathrev.unif.census,
      file = 'calc/mathrev-census.RData')
+print('Aggregate done')
 
 rm(list = ls())
